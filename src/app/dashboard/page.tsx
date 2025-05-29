54ed91d85c3c375623976e5e53d6afcda0e5155d
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2025-05-28 19:22:29 -0700

    frontend for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,33 @@
-'use client';
-
 import React from 'react'
+import ClassCard from '@/components/ClassCard';
+import SearchBar from '@/components/SearchBar';
+import Footer from '@/components/Footer';
 
 const page = () => {
   return (
-    <div>
-        <h1>Dashboard</h1>
+    <div className="bg-white text-gray-800 min-h-screen p-6">
+      <h1 className="text-xl font-bold mb-2">noteorbit</h1>
+      <hr className="border-gray-300 mb-4" />
+      <h1 className="text-3xl font-bold mb-2 mt-9">Hi, user</h1>
+
+      <SearchBar />
+
+      <h2 className="text-2xl font-semibold mt-8 mb-4">My Classes</h2>
+      <div className="flex flex-row gap-4 mb-6">
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+      </div>
+
+      <h2 className="text-2xl font-semibold mt-10 mb-4">Archived Classes</h2>
+      <div className="flex flex-row gap-4 mb-10">
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+        <ClassCard courseNumber="ECS162" courseName="Web Programming" />
+      </div>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer className="border-t pt-4 mt-10 text-black text-sm">
+            <h2 className="font-semibold text-xl">noteorbit</h2>
+            <p>@ Spring Quarter 2025 | ECS 162</p>
+        </footer>
+    );
+}
