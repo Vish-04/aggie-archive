407ff8906820fa8b01369b7a8897f10035954d8e
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2025-05-28 19:59:00 -0700

    Updated frontend for dashboard

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,12 @@
+'use client';
+import { useState } from "react";
+
 export default function SearchBar(){
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
-        <div className="w-full my-4">
-            <div className="flex items-center border border-gray-300 rounded-md px-3 py-2 bg-white">
+        <div className="w-full my-4" onBlur={() => setIsOpen(false)}>
+            <div className="flex items-center border border-gray-300 rounded-md px-3 py-2 bg-white" onClick={() => setIsOpen(true)}>
                 <img src="/search.png" alt="Search Icon" className="w-5 mr-3"></img>
                 <input
                     type="text"
@@ -9,6 +14,12 @@ export default function SearchBar(){
                     className="flex-grow outline-none bg-transparent text-black placeholder-gray-500"
                 />
             </div>
+
+            {isOpen && (
+                <div className="absolute left-0 right-0 mt-2 bg-white border border-gray-300 rounded-md shadow-md z-10 p-4">
+                <p className="text-gray-600">Classes are here</p>
+                </div>
+            )}
         </div>
     );
 }
