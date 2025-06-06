4c5809c9de546081b550fec5c8eca2a98df344ce
Ananya <ratakondaananya@gmail.com>
2025-06-06 15:55:23 -0700

    added main header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@
 import "./globals.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { DM_Sans } from 'next/font/google';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer"
 
 const dmSans = DM_Sans({
   subsets: ['latin'],
@@ -14,7 +16,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={dmSans.className}>
         <UserProvider>
+          <Header></Header>
           {children}
+          <Footer></Footer>
         </UserProvider>
       </body>
     </html>
