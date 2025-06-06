711d938239beea75b3ee6b9dd9726c8dca2e7bbe
Vishwa <vishwa@campus-024-214.ucdavis.edu>
2025-06-06 16:38:37 -0700

    git being wack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import "./globals.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { DM_Sans } from 'next/font/google';
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import Footer from "@/components/Footer"
 
 const dmSans = DM_Sans({
