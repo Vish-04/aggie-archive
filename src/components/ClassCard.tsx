1e77a20ee392eab505e8c7936ca56cf65a261ffa
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2025-06-09 18:46:42 -0700

    Added exit button

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -33,6 +33,7 @@ export default function ClassCard({course, onToggleClass, onToggleArchive, isArc
 					{/* if a current class, have archive button. if an unarchived class, have an unarchive button. */}
 					<div className="">
 						{!isArchived(course.course_code) ? (
+							<div>
 							<button className="pointer-cursor" onClick={(e) => {
 								e.stopPropagation();
 								onToggleArchive(course);
@@ -44,7 +45,14 @@ export default function ClassCard({course, onToggleClass, onToggleArchive, isArc
 										fill="black" stroke="#483183"/>
 								</svg>
 							</button>
+							<button className="pointer-cursor" onClick={(e) => {e.stopPropagation(); onToggleClass(course) }}>
+								<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path fillRule="evenodd" clipRule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#0F1729"/>
+								</svg>
+							</button>
+							</div>
 						) : (
+							<div>
 							<button className="pointer-cursor" onClick={(e) => {
 								e.stopPropagation();
 								onToggleArchive(course);
@@ -59,6 +67,12 @@ export default function ClassCard({course, onToggleClass, onToggleArchive, isArc
 												fill="#483183"/>
 								</svg>
 							</button>
+							<button className="pointer-cursor" onClick={(e) => {e.stopPropagation(); onToggleArchive(course) }}>
+								<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path fillRule="evenodd" clipRule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#0F1729"/>
+								</svg>
+							</button>
+							</div>
 						)}
 					</div>
 				</div>
