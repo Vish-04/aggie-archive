a9644c1c9df82857b9297a99a1862789c12200a9
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2025-06-10 19:10:31 -0700

    updated header tests

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,26 +1,51 @@
-import { render, screen } from '@testing-library/react'
-import Header from '../FinalHeader'
+import { render, screen, fireEvent } from '@testing-library/react';
+import Header from '../FinalHeader';
+import { useUser as mockUseUser } from '@auth0/nextjs-auth0/client';
 
-// set user to nil
+
 jest.mock('@auth0/nextjs-auth0/client', () => ({
-  useUser: () => ({
-    user: null,
-    isLoading: false,
-  }),
-}))
-
-// render header when no user is logged in
-test('render header', () => {
-  render(<Header />)
-  expect(screen.getByText(/log in/i)).toBeInTheDocument()
-  expect(screen.getByText(/sign up/i)).toBeInTheDocument()
-})
-
-// render all logos
-test('render header logo', () => {
-    render(<Header />)
-    const logo = screen.getAllByAltText('Noteorbit logo')
-    expect(logo.length).toBeGreaterThan(0)
-})
+  useUser: jest.fn(),
+}));
 
 
+describe('Header component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders header when user is not logged in', () => {
+    mockUseUser.mockImplementation(() => ({
+      user: null,
+      isLoading: false,
+    }));
+
+    render(<Header />);
+    expect(screen.getByText(/log in/i)).toBeInTheDocument();
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument();
+  });
+
+  test('renders logo', () => {
+    mockUseUser.mockImplementation(() => ({
+      user: null,
+      isLoading: false,
+    }));
+
+    render(<Header />);
+    const logo = screen.getAllByAltText('Noteorbit logo');
+    expect(logo.length).toBeGreaterThan(0);
+  });
+
+  test('renders header when user is logged in', () => {
+    mockUseUser.mockImplementation(() => ({
+      user: { name: 'Test User', email: 'test@example.com' },
+      isLoading: false,
+    }));
+
+    render(<Header />);
+    expect(screen.getByText('Test User')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Test User'));
+
+    expect(screen.getByText(/sign out/i)).toBeInTheDocument();
+  });
+});
